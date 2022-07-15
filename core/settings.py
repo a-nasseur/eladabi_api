3fefbb0649e6890458d54cec95a02016ba70dbde
abdelhak <a.nasseur.dev@gmail.com>
2022-07-15 12:56:00 +0100

    runtime file added

--- core/settings.py
+++ core/settings.py
@@ -7,6 +7,7 @@ from django import conf
 import django_heroku
 import dj_database_url
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
